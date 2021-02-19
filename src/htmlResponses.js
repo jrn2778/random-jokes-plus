1331ab20a3ea87fe100e3f0c4257fbf24620a3d8
jrn2778 <jrn2778@rit.edu>
2021-02-19 11:26:15 -0500

    Completed phase 4

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -4,7 +4,7 @@ const errorPage = fs.readFileSync(`${__dirname}/../client/error.html`);
 const defaultStyles = fs.readFileSync(`${__dirname}/../client/default-styles.css`);
 
 const get404Response = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.writeHead(404, { 'Content-Type': 'text/html' });
   response.write(errorPage);
   response.end();
 };
